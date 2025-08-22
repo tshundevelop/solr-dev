67f2b1adba0783df0f1dcc04218a787545ea4949
tshundevelop <tshun5982@gmail.com>
2025-08-22 16:14:51 +0900

    try vector search

--- java/src/main/java/Sample.java
+++ java/src/main/java/Sample.java
@@ -9,8 +9,9 @@ public class Sample {
 	public static void main(String[] args) {
 		String solrUrl = "http://solr:8983/solr/example"; // docker-composeのsolrサービス名に合わせる
 		try (SolrClient solr = new HttpSolrClient.Builder(solrUrl).build()) {
-			SolrQuery query = new SolrQuery();
-			query.setQuery("*:*"); // すべてのドキュメントを検索
+			SolrQuery query = new SolrQuery("text:生物"); // クエリを設定
+			query.setFields("id"); // 取得するフィールドを指定
+			// query.setQuery("text:生物"); // すべてのドキュメントを検索
 			QueryResponse response = solr.query(query);
 			SolrDocumentList docs = response.getResults();
 			System.out.println("Found " + docs.getNumFound() + " documents");
